84103be532dde367fd8491f11734bf19d189ce4f
jayjnu <doublejnu@gmail.com>
2021-04-17 16:09:42 +0900

    feat(create-cli): add default yes argvs

--- packages/create-cli/src/argvs.ts
+++ packages/create-cli/src/argvs.ts
@@ -1,10 +1,11 @@
 import {Command} from 'commander';
 
-
 export function parse(argvs: string[]) {
   const program = new Command();
 
-  program.option('--git', 'create git cli project', true);
+  program
+    .option('--git', 'create git cli project', true)
+    .option('-y, --yes', 'create with default config', false);
 
-  return program.parse(argvs);
+  return program.parse(argvs).opts();
 }
