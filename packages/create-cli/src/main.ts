739aecf9cf8fa584343da1c5e782facff06f4267
jayjnu <doublejnu@gmail.com>
2021-04-18 17:18:56 +0900

    build(create-cli): remove rollup
    handling dependencies is painful

--- packages/create-cli/src/main.ts
+++ packages/create-cli/src/main.ts
@@ -1,7 +1,13 @@
 import {prompt} from './argvs';
+import Listr from 'listr';
 
-export async function cli(argv: string[]) {
+export async function cli(argv: string[] = process.argv) {
+  console.log(argv);
   const opts = await prompt(argv);
-  
-  console.log(opts);
+  const tasks = new Listr([
+    {
+      title: 'Git',
+      task: () => Promise.resolve('Initialized Git project')
+    }
+  ]);
 };
