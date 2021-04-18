a28dc2feb874ed05c6a2923c53c4170bd5c01160
jayjnu <doublejnu@gmail.com>
2021-04-18 17:32:44 +0900

    refactor(create-cli): add dummy tasks

--- packages/create-cli/src/main.ts
+++ packages/create-cli/src/main.ts
@@ -2,12 +2,37 @@ import {prompt} from './argvs';
 import Listr from 'listr';
 
 export async function cli(argv: string[] = process.argv) {
-  console.log(argv);
   const opts = await prompt(argv);
   const tasks = new Listr([
     {
-      title: 'Git',
-      task: () => Promise.resolve('Initialized Git project')
+      title: 'Git - initialize git project...',
+      task: () => {
+        return new Promise((res) => {
+          setTimeout(res, 5000);
+        });
+      }
+    },
+    {
+      title: 'NPM - install dependencies',
+      task: () => {
+        return new Promise((res) => {
+          setTimeout(res, 5000);
+        })
+      }
+    },
+    {
+      title: 'Template - Generating Scaffolds',
+      task: () => {
+        return new Promise((res) => {
+          setTimeout(res, 5000);
+        })
+      }
     }
   ]);
+
+  try {
+    await tasks.run();
+  } catch(e) {
+    console.error(e);
+  }
 };
